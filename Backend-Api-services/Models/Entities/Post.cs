499a23dd1611cc2a4a8b6a6ede52634a8f9ef5c4
battomi <nenebattomi@gmail.com>
2024-08-11 22:32:53 +0300

    Add Entities folder and move existing entity classes into it

---
+++ Backend-Api-services/Models/Entities/Post.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_Api_services.Models.Entities
+{
+    [Table("posts")] // Ensure this matches the table name in your database
+    public class Post
+    {
+        [Key]
+        public int post_id { get; set; }
+        public int user_id { get; set; }
+        public string caption { get; set; } = "";
+        public DateTime created_at { get; set; }
+        public bool is_public { get; set; } = true;
+        public int like_count { get; set; } = 0;
+        public int comment_count { get; set; } = 0;
+
+        public List<PostMedia> Media { get; set; } = new List<PostMedia>();
+
+        [ForeignKey("user_id")]
+        public Users User { get; set; }
+    }
+}
---
+++ Backend-Api-services/Models/Entities/PostMedia.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_Api_services.Models.Entities
+{
+    [Table("postmedia")]
+    public class PostMedia
+    {
+        [Key]
+        public int media_id { get; set; }
+        public string media_url { get; set; } = "";
+        public string media_type { get; set; } = "";
+        public int post_id { get; set; } // Foreign key to associate with Post
+
+        // Navigation property to post
+        [ForeignKey("post_id")]
+        public Post post { get; set; }
+    }
+}
