4107a830a184d7a3d9e65c9275a634aa13e64abb
battomi <nenebattomi@gmail.com>
2024-08-15 21:42:10 +0300

    post request dto

---
+++ Backend-Api-services/Models/DTOs/PostRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Backend_Api_services.Models.DTOs
+{
+    public class PostRequest
+    {
+        public int user_id { get; set; }
+        public string caption { get; set; } = "";
+        public bool is_public { get; set; } = true;
+        public List<PostMediaRequest> Media { get; set; } = new List<PostMediaRequest>();
+    }
+
+    public class PostMediaRequest
+    {
+        public string media_url { get; set; } = "";
+        public string media_type { get; set; } = "";
+    }
+}
