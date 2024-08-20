2bbfc02eb262ca78589140433904dd01fbb3068c
battomi <nenebattomi@gmail.com>
2024-08-20 20:47:39 +0300

    added entity like

---
+++ Backend-Api-services/Models/Entities/Like.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_Api_services.Models.Entities
+{
+    [Table("likes")]
+    public class Like
+    {
+        [Key]
+        public int like_id { get; set; }
+        public int post_id { get; set; }
+        public int user_id { get; set; }
+        public DateTime created_at { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("post_id")]
+        public Post Post { get; set; }
+
+        [ForeignKey("user_id")]
+        public Users User { get; set; }
+    }
+}
