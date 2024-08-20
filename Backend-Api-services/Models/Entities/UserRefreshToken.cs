95e48b2fad997f9cc2ec43b67610127f2eb22722
battomi <nenebattomi@gmail.com>
2024-08-20 20:46:50 +0300

    added entity for table userfereshtoken

---
+++ Backend-Api-services/Models/Entities/UserRefreshToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_Api_services.Models.Entities
+{
+    [Table("userrefreshtokens")]
+    public class UserRefreshToken
+    {
+        [Key]
+        public int id { get; set; }
+
+        [ForeignKey("User")]
+        public int userid { get; set; }
+
+        [Required]
+        public string token { get; set; }
+
+        [Required]
+        public DateTime expiresat { get; set; }
+
+        [Required]
+        public DateTime createdat { get; set; } = DateTime.UtcNow;
+
+        public Users User { get; set; }
+    }
+}
