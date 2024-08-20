5df2284fd5b2dfcf55a97cf14a748b9bd3ea50c9
battomi <nenebattomi@gmail.com>
2024-08-20 20:48:22 +0300

    added data transfer object for userfefreshtokemdto

---
+++ Backend-Api-services/Models/DTOs/UserRefreshTokenDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_Api_services.Models.DTOs
+{
+    public class UserRefreshTokenDTO
+    {
+        public int id { get; set; }
+        public int userid { get; set; }
+        public string token { get; set; }
+        public DateTime expiresat { get; set; }
+        public DateTime createdat { get; set; }
+    }
+}
