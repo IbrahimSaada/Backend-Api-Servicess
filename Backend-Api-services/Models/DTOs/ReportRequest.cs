2673ca461360c4414b6dc73b397b85fc65f2c4bf
battomi <nenebattomi@gmail.com>
2024-08-27 00:07:41 +0300

    add dto for report request and report response

---
+++ Backend-Api-services/Models/DTOs/ReportRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend_Api_services.Models.DTOs
+{
+    public class ReportRequest
+    {
+        public int ReportId { get; set; }
+        public int ReportedBy { get; set; }
+        public string ReportedByUsername { get; set; } = "";  // Add the username for clarity
+        public int ReportedUser { get; set; }
+        public string ReportedUserUsername { get; set; } = "";  // Add the username for clarity
+        public string content_type { get; set; } = "";
+        public int ContentId { get; set; }
+        public string ReportReason { get; set; } = "";
+        public string ReportStatus { get; set; } = "Pending";
+        public string SeverityLevel { get; set; } = "Medium";
+        public string? resolution_details { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ResolvedAt { get; set; }  // Nullable in case it's unresolved
+    }
+}
---
+++ Backend-Api-services/Models/DTOs/ReportResponse.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Backend_Api_services.Models.DTOs
+{
+    public class ReportResponse
+    {
+        public int ReportedBy { get; set; }
+        public int ReportedUser { get; set; }
+        public string ContentType { get; set; }  // Use the enum here
+        public int ContentId { get; set; }
+        public string ReportReason { get; set; } = "";
+        public string? resolution_details { get; set; }
+    }
+}
