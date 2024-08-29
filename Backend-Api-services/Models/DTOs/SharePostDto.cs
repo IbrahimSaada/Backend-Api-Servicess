871ecdfb7afec1ff44d0907a923d8764f53507bb
battomi <nenebattomi@gmail.com>
2024-08-29 10:05:01 +0300

    added shared posts dto's

---
+++ Backend-Api-services/Models/DTOs/SharePostDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend_Api_services.Models.DTOs
+{
+    public class SharePostDto
+    {
+        [Required]
+        public int UserId { get; set; }  // The ID of the user sharing the post
+
+        [Required]
+        public int PostId { get; set; }  // The ID of the post being shared
+        public string? Comment { get; set; }
+    }
+}
---
+++ Backend-Api-services/Models/DTOs/SharedPostDetailsDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Backend_Api_services.Models.DTOs
+{
+    public class SharedPostDetailsDto
+    {
+        public int ShareId { get; set; }  // The ID of the share action
+
+        public int SharerId { get; set; }  // The ID of the user who shared the post
+        public string SharerUsername { get; set; }  // The username of the user who shared the post
+        public string SharerProfileUrl { get; set; }  // The profile URL of the user who shared the post
+
+        public int PostId { get; set; }  // The ID of the shared post
+        public string PostContent { get; set; }  // The content of the shared post
+        public DateTime PostCreatedAt { get; set; }  // The timestamp when the post was created
+
+        public List<PostMediaDto> Media { get; set; }  // A list of media associated with the post
+
+        public DateTime SharedAt { get; set; }  // The timestamp when the post was shared
+        public string? Comment { get; set; }  // Optional comment added by the sharer
+
+        public string OriginalPostUserUrl { get; set; }  // The profile URL of the original post's author
+    }
+
+    public class PostMediaDto
+    {
+        public string MediaUrl { get; set; }  // The URL of the media
+        public string MediaType { get; set; }  // The type of media (e.g., image, video)
+    }
+}
