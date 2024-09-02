cfa3a88b040d1d9b736685e396557ad9e5f17a8d
battomi <nenebattomi@gmail.com>
2024-09-02 22:00:40 +0300

    added storieyview request and response dto

---
+++ Backend-Api-services/Models/DTOs/StoryViewRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Api_services.Models.DTOs
+{
+    public class StoryViewRequest
+    {
+        public int story_id { get; set; }
+        public int viewer_id { get; set; }
+    }
+}
---
+++ Backend-Api-services/Models/DTOs/StoryViewerResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend_Api_services.Models.DTOs
+{
+    public class StoryViewerResponse
+    {
+        public int viewer_id { get; set; }
+        public string fullname { get; set; }
+        public string profile_pic { get; set; }
+        public DateTime viewed_at { get; set; }
+    }
+}
