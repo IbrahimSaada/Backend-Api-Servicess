7ab7ff43849cf8d1dc09446ceac7c8c94d375c77
battomi <nenebattomi@gmail.com>
2024-09-10 15:53:10 +0300

    added signture class for api secuirty this class can be used by multiple controllers

---
+++ Backend-Api-services/Services/SignatureService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend_Api_services.Services
+{
+    public class SignatureService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SignatureService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool ValidateSignature(string receivedSignature, string dataToSign)
+        {
+            var secretKey = _configuration["AppSecretKey"];
+
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
+                var computedSignature = Convert.ToBase64String(computedHash);
+
+                return CryptographicOperations.FixedTimeEquals(
+                    Encoding.UTF8.GetBytes(computedSignature),
+                    Encoding.UTF8.GetBytes(receivedSignature)
+                );
+            }
+        }
+    }
+}
