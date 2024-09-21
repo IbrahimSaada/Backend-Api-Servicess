6a1657b7656e1ccf56a2467cde5be14e6c7d870f
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-09-21 20:06:27 +0300

    added bookmar entity

---
+++ Backend-Api-services/Models/Entities/Bookmark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Backend_Api_services.Models.Entities
+{
+    [Table("bookmark")]
+    public class Bookmark
+    {
+        [Key]
+        public int bookmark_id { get; set; }
+
+        public int user_id { get; set; }
+        public int post_id { get; set; }
+
+        [ForeignKey("user_id")]
+        [JsonIgnore]
+        public Users users { get; set; }
+
+        [ForeignKey("post_id")]
+        [JsonIgnore]
+        public Post post { get; set; }
+    }
+}
