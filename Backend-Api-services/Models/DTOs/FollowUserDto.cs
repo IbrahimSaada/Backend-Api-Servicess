724166d69ce364aaaefd285b72d6b340109bad22
battomi <nenebattomi@gmail.com>
2024-09-24 16:56:38 +0300

    changed variables names for better readablity

--- Backend-Api-services/Models/DTOs/FollowUserDto.cs
+++ Backend-Api-services/Models/DTOs/FollowUserDto.cs
@@ -2,7 +2,7 @@
 {
     public class FollowUserDto
     {
-        public int user_id { get; set; } // User initiating the follow request
+        public int followed_user_id { get; set; } // User initiating the follow request
         public int follower_user_id { get; set; } // User being followed
         public bool is_public { get; set; } = true; // If the following is public
     }
