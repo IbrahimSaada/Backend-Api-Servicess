7cd12d965bcb771301806ef675985f1eec8a9121
battomi <nenebattomi@gmail.com>
2024-10-09 00:25:29 +0300

    added dto for follow status + aprroval status

---
+++ Backend-Api-services/Models/DTOs/UpdateFollowStatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_Api_services.Models.DTOs
+{
+    public class UpdateFollowStatusDto
+    {
+        public int followed_user_id { get; set; } // The user being followed
+        public int follower_user_id { get; set; } // The user who initiated the follow request
+        public string approval_status { get; set; } // The new approval status (approved, declined, pending)
+    }
+}
