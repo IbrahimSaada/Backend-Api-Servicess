606ab2a3bb7e7fa590e60ce77bc847456296c732
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-10-09 15:38:07 +0300

    add api to fetch post

--- Backend-Api-services/Controllers/Controller-Admin/PostManagementController.cs
+++ Backend-Api-services/Controllers/Controller-Admin/PostManagementController.cs
@@ -1,4 +1,5 @@
 ﻿using Backend_Api_services.Models.Data;
+using Backend_Api_services.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,43 @@ namespace Backend_Api_services.Controllers
             var postCount = await _context.Posts.CountAsync();
 
             return Ok(new { TotalPosts = postCount });
+        }
+        // GET: api/Posts/display
+        [HttpGet("display")]
+        [AllowAnonymous] // Allows anyone to access this endpoint without authentication
+        public async Task<ActionResult<IEnumerable<PostResponse>>> GetPosts()
+        {
+            // Fetch all public posts from the database
+            var posts = await _context.Posts
+                                      .Include(p => p.User)
+                                      .Include(p => p.Media)
+                                      .Where(p => p.is_public) // Only public posts
+                                      .OrderByDescending(p => p.created_at)
+                                      .ToListAsync();
+
+            // Convert posts to PostResponse DTO format
+            var postResponses = posts.Select(post => new PostResponse
+            {
+                post_id = post.post_id,
+                caption = post.caption,
+                comment_count = post.comment_count,
+                created_at = post.created_at,
+                is_public = post.is_public,
+                like_count = post.like_count,
+                user_id = post.user_id,
+                fullname = post.User.fullname,
+                profile_pic = post.User.profile_pic,
+                Media = post.Media.Select(media => new PostMediaResponse
+                {
+                    media_id = media.media_id,
+                    media_url = media.media_url,
+                    media_type = media.media_type,
+                    post_id = media.post_id
+                }).ToList()
+            }).ToList();
+
+            // Return the list of posts
+            return Ok(postResponses);
         }
     }
 }
