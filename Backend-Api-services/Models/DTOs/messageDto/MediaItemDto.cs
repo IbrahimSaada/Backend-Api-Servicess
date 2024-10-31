b72859adc1a5f4c5838a8eedf664d98c99be811c
battomi <nenebattomi@gmail.com>
2024-10-31 13:38:24 +0200

    added dto for media_type handling

---
+++ Backend-Api-services/Models/DTOs/messageDto/MediaItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Api_services.Models.DTOs.messageDto
+{
+    public class MediaItemDto
+    {
+        public string MediaUrl { get; set; }
+        public string MediaType { get; set; }
+    }
+}
