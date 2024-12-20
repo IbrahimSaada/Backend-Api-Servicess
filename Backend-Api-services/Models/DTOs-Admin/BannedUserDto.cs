171951f1761d7d4aef7d4af744718792e67d6bdb
battomi <nenebattomi@gmail.com>
2024-12-20 11:08:00 +0200

    getallbanned users endpoint

---
+++ Backend-Api-services/Models/DTOs-Admin/BannedUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_Api_services.Models.DTOs_Admin
+{
+    public class BannedUserDto
+    {
+        public int UserId { get; set; }
+        public string BanReason { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
--- Backend-Api-services/Services/Interfaces/IBanService.cs
+++ Backend-Api-services/Services/Interfaces/IBanService.cs
@@ -1,8 +1,13 @@
-﻿public interface IBanService
+﻿
+using Backend_Api_services.Models.DTOs_Admin;
+
+public interface IBanService
 {
     Task<bool> BanUserAsync(int userId, string reason, DateTime? expiresAt = null);
     Task<bool> UnbanUserAsync(int userId);
     Task<bool> IsUserBannedAsync(int userId);
 
     Task<(bool IsBanned, string BanReason, DateTime? BanExpiresAt)> GetBanDetailsAsync(int userId);
+
+    Task<(List<BannedUserDto> BannedUsers, int TotalCount)> GetAllBannedUsersAsync(int page, int pageSize);
 }
